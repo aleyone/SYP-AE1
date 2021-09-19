692274b15f26e7fb7fc605cad85a39024e6f92f3
David Gil <dagipe01@floridauniversitaria.es>
2021-09-19 20:01:22 +0200

    AE1-4: Actividad 4. Factorial de 15

---
+++ AE1-4/src/es/aev1/ae4/AE4.java
@@ -0,0 +1,12 @@
+package es.aev1.ae4;
+
+public class AE4 {
+
+	public static void main(String[] args) {
+		long calculo=1, parametro=15;
+		for (int i=1; i<=parametro;i++) {
+			calculo*=i;
+		}
+		System.out.println (calculo);		
+	}
+}
