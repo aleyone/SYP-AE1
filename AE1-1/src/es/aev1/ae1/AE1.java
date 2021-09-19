7bca60e803d9e9b26b0bd18058efcc5070cb6fcb
David Gil <dagipe01@floridauniversitaria.es>
2021-09-19 18:55:28 +0200

    AE1-1. Commit de ejercicio 1: Hola Mundo

---
+++ AE1-1/src/es/aev1/ae1/AE1.java
@@ -0,0 +1,10 @@
+package es.aev1.ae1;
+
+public class AE1 {
+
+	public static void main(String[] args) {
+		App miapp = new App();
+		System.out.println(miapp.sayHello());
+	}
+
+}
---
+++ AE1-1/src/es/aev1/ae1/App.java
@@ -0,0 +1,9 @@
+package es.aev1.ae1;
+
+public class App {
+	
+	public String sayHello() {
+		return "Hola Mundo, qué fem";
+	}
+
+}
