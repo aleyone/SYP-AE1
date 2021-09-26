fc9749e0bc917010030424b2c999d1ebc3cc71c9
David Gil <dagipe01@floridauniversitaria.es>
2021-09-26 19:19:56 +0200

    AE1-6: Commit con comentarios

--- AE1-6/src/es/aev1/ae6/app.java
+++ AE1-6/src/es/aev1/ae6/app.java
@@ -5,6 +5,11 @@ import java.util.List;
 
 public class app {
 	
+	/**
+	 * OrdenaLista reordena la lista a la inversa creando una copia del array
+	 * @param numeros pasamos la lista de enteros
+	 * @return devolvemos la copia de la lista ordenada a la inversa
+	 */
 	public List<Integer> OrdenaLista (List<Integer> numeros){
 		List<Integer> copialista = new ArrayList<Integer>();
 		for (int k=numeros.size()-1;k>=0; k--) {
@@ -14,6 +19,11 @@ public class app {
 		return copialista;
 	}
 	
+	/**
+	 * SumaLista: suma valores de la lista de enteros
+	 * @param numeros: se recoge la lista de enteros
+	 * @return devuelve la suma
+	 */
 	public int SumaLista (List <Integer> numeros) {
 		int suma=0;
 		for (int l=0; l<numeros.size(); l++) {
